ac1e30e06161e900a640f77d3b818ebf625c93bf
Youngho Kook <dijinda@gmail.com>
2026-09-10 09:35:31 +0900

    최초 프로젝트 생성 커밋

---
+++ freepoint/src/main/java/kr/co/freepoint/FreepointApplication.java
@@ -0,0 +1,13 @@
+package kr.co.freepoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreepointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreepointApplication.class, args);
+    }
+
+}
---
+++ freepoint/src/test/java/kr/co/freepoint/FreepointApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.freepoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreepointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
